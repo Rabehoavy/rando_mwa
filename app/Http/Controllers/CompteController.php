5aae8df865b2a7e6ffc864856f12111cdeb44306
Rabehoavy <rabehoavy@users.noreply.github.com>
2019-03-13 06:42:20 +0100

    Création MCV Utilisateurs

---
+++ app/Http/Controllers/CompteController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CompteController extends Controller
+{
+    public function accueil()
+    {
+        return view('mon-compte');
+    }
+
+    public function deconnexion()
+    {
+        auth()->logout();
+
+        flash("Vous êtes maintenant déconnecté.")->success();
+
+        return redirect('/connexion');
+    }
+
+    public function modificationMotDePasse()
+    {
+        request()->validate([
+            'password' => ['required', 'confirmed', 'min:8'],
+            'password_confirmation' => ['required'],
+        ]);
+
+        // modifier l'utilisateur
+        auth()->user()->update([
+            'mot_de_passe' => bcrypt(request('password')),
+        ]);
+
+        flash("Votre mot de passe a bien été mis à jour.")->success();
+
+        return redirect('/mon-compte');
+
+    }
+
+}
---
+++ resources/views/inscription.blade.php
@@ -0,0 +1,43 @@
+@extends('layout')
+
+@section('contenu')
+<form action="/inscription" method="post" class="section">
+{{ csrf_field() }}
+
+    <div class="field">
+        <label class="label">Adresse e-mail</label>
+        <div class="control">
+            <input class="input" type="email" name="email" value="{{ old('email') }}">
+        </div>
+        @if($errors->has('email'))
+            <p class="help is-danger">{{ $errors->first('email') }}</p>
+        @endif
+    </div>
+
+    <div class="field">
+        <label class="label">Mot de passe</label>
+        <div class="control">
+            <input class="input" type="password" name="password">
+        </div>
+        @if($errors->has('password'))
+            <p class="help is-danger">{{ $errors->first('password') }}</p>
+        @endif
+    </div>
+
+    <div class="field">
+        <label class="label">Mot de passe (confirmation)</label>
+        <div class="control">
+            <input class="input" type="password" name="password_confirmation">
+        </div>
+        @if($errors->has('password_confirmation'))
+            <p class="help is-danger">{{ $errors->first('password_confirmation') }}</p>
+        @endif
+    </div>
+
+    <div class="field">
+        <div class="control">
+            <button class="button is-link" type="submit">M'inscrire</button>
+        </div>
+    </div>
+</form>
+@endsection
