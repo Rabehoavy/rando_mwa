0134dfb20e5d1c8b168342c1f4561d41a03caca0
Manga974 <mangualam.cany@gmail.com>
2019-03-13 10:04:15 +0400

    Test File Javascript
    JS pas détecté

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -8,7 +8,7 @@
 
 
         <link href="{{ asset('css/style.css') }}" rel="stylesheet">
-        <script type="text/javascript" src="{{ asset('public/js/main.js') }}"></script>
+        <script type="text/javascript" src="{{ asset('js/todolist.js') }}"></script>
 
         <!--Liens CDN Bootstrap-->
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
